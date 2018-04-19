87038a5eb09e706b3b79c31e93c6f635f3415317
pojda <thiago.pojda@gmail.com>
2018-04-19 15:44:58 -0300

    ** Initial Commit
    First commit

---
+++ URLStatus.py
@@ -0,0 +1,68 @@
+import requests
+import json
+from time import sleep
+
+class URLStatus:
+    user_agents = {}
+    urls = []
+    errors = []
+
+    def __init__(self, args):
+        self.user_agents = self._getUAs(args)
+        self.urls = self._getUrls(args)
+        self.runTests()
+        print(self.errors)
+
+
+    def runTests(self):
+        for url in self.urls:
+            for device in self.user_agents:
+                for ua in self.user_agents[device]:
+                    self._test(url, ua)
+
+    def _test(self, url, ua):
+        headers = {'user-agent':ua}
+        print "\nTesting URL: %s With UA: %s" % (url, ua)
+        for n in range(5): # sometimes we get ioerror and need to retry
+            try:
+                r = requests.get(url, headers=headers)
+                print("'{}' >> URL FINAL >> '{}'".format(url, r.url))
+                print("Status {}, Historico: {}".format(r.status_code, r.history))
+                sleep(0.5)
+                break
+
+            except requests.exceptions.TooManyRedirects:
+                print("*** REDIRECT LOOP!! ***")
+                print("*** REDIRECT LOOP!! ***")
+                self.errors.append([url, ua, "REDIRECT LOOP"])
+                break
+                #exit(1)
+            
+            except requests.exceptions.RequestException as e:
+                print("Request error: {}".format(e))
+                self.errors.append([url, ua, "REQUEST ERROR"])
+                exit(1)
+
+            except IOError as e:
+                print("Request error, retrying... {}".format(e))
+                self.errors.append([url, ua, "IOERROR"])
+
+    def _getUAs(self, args):
+        if args.user_agents_file:
+            if ".json" in args.user_agents_file:
+                user_agents = json.load(open(args.user_agents_file,'r'))
+            else:
+                print("file type not yet supported")
+                exit()
+        else:
+            user_agents = json.load(open("uas.json",'r'))
+        
+        return user_agents
+
+    def _getUrls(self, args):
+        if args.urls_file:
+            with open(args.urls_file) as f:
+                urls = [line.rstrip() for line in f.readlines()]
+        else:
+            urls = [args.url]
+        return urls
---
+++ url-tester.py
@@ -0,0 +1,34 @@
+import argparse
+import URLStatus as us
+
+parser = argparse.ArgumentParser("simple_example")
+parser.add_argument("-a", "--all",
+                    help="(default) will validate on all user agent platforms (desktop, mobile and tablet)", 
+                    action="store_true",
+                    default=True)
+
+parser.add_argument("-d", "--desktop",
+                    help="Validates using Desktop user agent", 
+                    action="store_true")
+
+parser.add_argument("-m", "--mobile", 
+                    help="Validates using Mobile user agent", 
+                    action="store_true")
+
+parser.add_argument("-t", "--tablet", 
+                    help="Validates using Tablet user agent", 
+                    action="store_true")
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument("--urls-file",
+                    help="If specified, will read urls validation from file",
+                    default=False)
+group.add_argument("--url",
+                    help="The URL to be tested")
+
+parser.add_argument("--user-agents-file",
+                    help="If specified, will read User Agents from file (implies -a)",
+                    default=False)
+
+args = parser.parse_args()
+tester = us.URLStatus(args)
